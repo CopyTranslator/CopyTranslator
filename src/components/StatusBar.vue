fd19e231649d5f147aa78b7535908e3c88cb6418
Elliott Zheng <18350299690@163.com>
2019-01-20 01:27:57 +0800

    on mouse move

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="draggable" v-on:dblclick="minify" v-on:mousedown="isFollow=!isFollow" v-on:mouseup="isFollow=!isFollow" onmousemove="moveWindow"></div>
+    <div class="draggable" v-on:dblclick="minify" v-on:mousedown="mouseDown" v-on:mouseup="isFollow=!isFollow" v-on:mousemove="moveWindow"></div>
 </template>
 
 <script>
 export default {
   name: "StatusBar",
   data: function() {
+    console.log("in");
+
     return {
-      isFollow: false
+      isFollow: false,
+      x: 0,
+      y: 0
     };
   },
   methods: {
@@ -15,8 +19,21 @@ export default {
       console.log(event);
       console.log("最小化");
     },
+    mouseDown(event) {
+      this.x = event.screenX;
+      this.y = event.screenY;
+      this.isFollow = true;
+    },
     moveWindow(event) {
-      console.log(event);
+      if (this.isFollow) {
+        let x = event.screenX;
+        let y = event.screenY;
+        let dx = x - this.x;
+        let dy = y - this.y;
+        console.log(dx, dy);
+        this.x = x;
+        this.y = y;
+      }
     }
   }
 };
@@ -26,7 +43,7 @@ export default {
 .draggable {
   height: 15px;
   background: red;
-  -webkit-user-select: none;
-  -webkit-app-region: drag;
+  /*-webkit-user-select: none;*/
+  /*-webkit-app-region: drag;*/
 }
 </style>

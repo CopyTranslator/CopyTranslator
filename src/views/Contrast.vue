22222db4e7c97967fca4ca8d964edde8d82b181d
Elliott Zheng <admin@hypercube.top>
2019-02-18 21:39:28 +0800

    window resize

--- src/views/Contrast.vue
+++ src/views/Contrast.vue
@@ -1,23 +1,23 @@
 <template>
-    <div v-on:contextmenu="openMenu('Focus')" style="background:black;" ref="big">
-      <StatusBar></StatusBar>
-      <div class="contrast"  ref="container">
-        <div class="a">
+    <div v-on:contextmenu="openMenu('Focus')">
+      <StatusBar ref="bar"></StatusBar>
+      <div class="contrast">
+        <div class="textPanel">
           <textarea :style="area" v-if="sharedResult" v-model="sharedResult.src"></textarea>
      <textarea :style="area" v-if="sharedResult" v-model="sharedResult.result">
   
      </textarea>
      </div>
-        <div class="b">
-      <mu-button full-width color="primary" @click="logs">{{$t("translate")}}</mu-button>
-      <mu-button full-width @click="changeMode('Focus')">{{$t("switchMode")}}</mu-button>
-      <mu-button full-width @click="changeMode('Settings')">{{$t("settings")}}</mu-button>
-       <mu-select :label="$t('sourceLanguage')"  v-model="source" full-width >
+        <div class="settingPanel">
+           <mu-select :label="$t('sourceLanguage')"  v-model="source" full-width >
           <mu-option v-for="lang in languages"  :key="lang" :label="lang" :value="lang"></mu-option>
       </mu-select>
       <mu-select :label="$t('targetLanguage')" v-model="target" full-width>
           <mu-option v-for="lang in languages"  :key="lang" :label="lang" :value="lang"></mu-option>
       </mu-select>
+      <mu-button full-width color="primary" @click="open">{{$t("translate")}}</mu-button>
+      <mu-button full-width @click="changeMode('Focus')">{{$t("switchMode")}}</mu-button>
+      <mu-button full-width @click="changeMode('Settings')">{{$t("settings")}}</mu-button>
         </div>
     </div>
 
@@ -35,12 +35,16 @@ export default {
   data: function() {
     return {
       loaded: false,
-      size: this.$controller.config.values.contrast.fontSize
+      size: this.$controller.config.values.contrast.fontSize,
+      windowHeight: window.screen.height
     };
   },
   computed: {
     area() {
-      return `fontSize:${this.size.toString()}px;width:100%;height:45vh`;
+      return `fontSize:${this.size.toString()}px;width:100%;height:${(this
+        .windowHeight -
+        this.barHeight) /
+        2}px`;
     }
   },
   components: {
@@ -51,11 +55,21 @@ export default {
     this.$nextTick(() => {
       this.languages = this.$controller.translator.getLanguages();
     });
+    console.log("???");
+    window.addEventListener("resize", this.syncHeight);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.myEventHandler);
   },
   methods: {
-    logs() {
-      console.log(this.$refs.big);
-      console.log(this.$refs.container);
+    syncHeight() {
+      this.windowHeight = window.screen.availHeight;
+      console.log(this.windowHeight);
+    },
+    open() {
+      window.open(
+        "https://www.cnblogs.com/top5/archive/2009/05/07/1452135.html"
+      );
     }
   }
 };
@@ -67,16 +81,5 @@ export default {
   display: grid;
   grid-template-columns: 6fr 1fr;
   width: 100%;
-  background: red;
-}
-.a {
-  width: 100%;
-
-  background: blue;
-}
-
-.b {
-  margin-right: 0%;
-  background: orange;
 }
 </style>

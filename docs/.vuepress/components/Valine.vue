b2434a68a3f4b9a89930eb5149e9ecc64d485b32
Elliott Zheng <admin@hypercube.top>
2019-03-06 11:12:51 +0800

    add valine comment system for docs

---
+++ docs/.vuepress/components/Valine.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="vcomments"></div>
+</template>
+
+<script>
+export default {
+  name: 'Valine',
+  mounted: function(){
+    // require window 
+    const Valine = require('valine');
+    if (typeof window !== 'undefined') {
+      this.window = window
+      window.AV = require('leancloud-storage')
+      
+    }
+     
+    new Valine({
+      el: '#vcomments' ,
+      appId: '6NloYo1rdCw6Rsg6jhcogzje-gzGzoHsz',// your appId
+      appKey: 'fXEoGtiwFukkdeGkWkizPsBY', // your appKey
+      notify:false, 
+      verify:false, 
+      avatar:'mm', 
+      placeholder: 'just go go' 
+    });
+  },
+}
+</script>

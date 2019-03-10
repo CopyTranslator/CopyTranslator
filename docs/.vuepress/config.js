660cd7f98a2eaf8529c40b568c09c82bc675f6cc
Elliott Zheng <admin@hypercube.top>
2019-03-10 15:30:20 +0800

    fix edit link error

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,23 +18,16 @@ module.exports = {
     },
   },
   themeConfig: {
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'copyrtanslator/copyrtanslator',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码', 
-
-    // 以下为可选的编辑链接选项
-
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    // 假如文档不是放在仓库的根目录下：
-    docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！',
+      // 假如你的文档仓库和项目本身不在一个仓库：
+      docsRepo: 'copytranslator/copytranslator',
+      // 假如文档不是放在仓库的根目录下：
+      docsDir: 'docs',
+      // 假如文档放在一个特定的分支下：
+      docsBranch: 'master',
+      // 默认是 false, 设置为 true 来启用
+      editLinks: true,
+      // 默认为 "Edit this page"
+      editLinkText: '帮助我们改善此页面！',
       lastUpdated: 'Last Updated', // string | boolean
       nav: [
         { text: '主页', link: '/' },

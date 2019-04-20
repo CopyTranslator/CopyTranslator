3b858728d806fb234af2211374effbe1ebd788d2
Elliott Zheng <admin@hypercube.top>
2019-04-20 20:30:59 +0800

    轻易不要改constants.json 的内容，不然会出问题

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -1,19 +1,14 @@
 const _ = require("lodash");
-function getAttr(attr: string): any {
-  return process.env["npm_package_" + attr];
-}
-
-export const constants: { [key: string]: any } = {
-  appName: getAttr("name"),
+export const constants = {
+  appName: "CopyTranslator",
   nickName: "Zouwu",
-  version: `v${getAttr("version")}`,
+  version: "8.2.0",
   stage: "alpha",
   wiki: "https://copytranslator.github.io/guide",
   homepage: "https://copytranslator.github.io",
   downloadPage:
     "https://copytranslator.github.io/guide/download.html#%E4%B8%8B%E8%BD%BD"
 };
-
 export const version = _.join(
   [constants.version, constants.nickName, constants.stage],
   " "

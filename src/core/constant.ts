c50f2bba6b0fa9ef53a7a9fb64de3aef4cc5049c
Elliott Zheng <admin@hypercube.top>
2019-12-18 21:18:35 +0800

    optimize the logic of translate and dictionary

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -2,7 +2,7 @@ export const constants = {
   appName: "CopyTranslator",
   nickName: "寒渐",
   version: "v9.0.0",
-  stage: "bate3",
+  stage: "",
   wiki: "https://copytranslator.github.io/guide",
   homepage: "https://copytranslator.github.io",
   downloadPage:

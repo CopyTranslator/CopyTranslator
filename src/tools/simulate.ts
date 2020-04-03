4a2f1fe4d6c42920321eb7074028e844f1ffbcf0
lizishan <m13677066509@gmail.com>
2020-04-03 21:08:27 +0800

    mac platform build

--- src/tools/simulate.ts
+++ src/tools/simulate.ts
@@ -1,13 +1,24 @@
 const robot = require("robotjs");
 const backup = "";
+import os from "os";
+const isMac: boolean = os.platform() == "darwin";
+
+/*function simulateCopy() {
+  // robot.keyTap("C", "control");
+  robot.keyTap("C", ["control", "command"]);
+}*/
 
 function simulateCopy() {
-  robot.keyTap("C", "control");
+  robot.keyTap("C", isMac ? "command" : "control");
+}
+function simulatePaste() {
+  robot.keyTap("V", isMac ? "command" : "control");
 }
 
-function simulatePaste() {
-  robot.keyTap("V", "control");
-}
+/*function simulatePaste() {
+  // robot.keyTap("V", "control");
+  robot.keyTap("V", ["control", "command"]);
+}*/
 
 export default {
   copy: simulateCopy,

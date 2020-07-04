98466023e7c6fb1fc3da4cf3643ff380f5e146be
Elliott Zheng <admin@hypercube.top>
2020-07-04 20:04:21 +0800

    clean src

--- src/tools/translate/constants.ts
+++
@@ -1,9 +0,0 @@
-export const translatorTypes = [
-  "baidu",
-  "google",
-  "sogou",
-  "caiyun",
-  "tencent",
-  "youdao"
-] as const;
-export type TranslatorType = typeof translatorTypes[number];

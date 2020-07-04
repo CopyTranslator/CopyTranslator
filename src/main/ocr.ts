c29d65c69e219bf6bd8a1b92e3ec54f5b401f950
Elliott Zheng <admin@hypercube.top>
2020-07-04 21:47:44 +0800

    fix #249

--- src/main/ocr.ts
+++ src/main/ocr.ts
@@ -1,8 +1,23 @@
 const AipOcrClient = require("baidu-aip-sdk").ocr;
 import { examToken } from "@/common/translate/token";
 import eventBus from "@/common/event-bus";
+import conf from "@/common/configuration";
+import { defaultConfig } from "./views/utils";
+import { Language } from "@opentranslate/languages";
 const ShortcutCapture = require("shortcut-capture");
 export const shortcutCapture = new ShortcutCapture();
+
+type LanguageType =
+  | "CHN_ENG"
+  | "ENG"
+  | "POR"
+  | "FRE"
+  | "GER"
+  | "ITA"
+  | "SPA"
+  | "RUS"
+  | "JAP"
+  | "KOR";
 
 export class Recognizer {
   client: any;
@@ -32,13 +47,45 @@ export class Recognizer {
     shortcutCapture.shortcutCapture();
   }
 
+  getLanguage(): LanguageType {
+    const srcLang: Language = conf.get("sourceLanguage");
+    switch (srcLang) {
+      case "en":
+        return "CHN_ENG";
+      case "zh-CN":
+        return "CHN_ENG";
+      case "pt":
+        return "POR";
+      case "fr":
+        return "FRE";
+      case "de":
+        return "GER";
+      case "it":
+        return "ITA";
+      case "es":
+        return "SPA";
+      case "ru":
+        return "RUS";
+      case "ja":
+        return "JAP";
+      case "ko":
+        return "KOR";
+      default:
+        return "CHN_ENG";
+    }
+  }
+
   recognize(image: string) {
     if (!this.client) {
       return;
     }
     image = image.substring(image.indexOf(",") + 1);
+
     this.client
-      .generalBasic(image)
+      .generalBasic(image, {
+        language_type: this.getLanguage(),
+        detect_language: "true",
+      })
       .then(function (result: { words_result: Array<{ words: string }> }) {
         const text = result.words_result
           .map((item) => item["words"])

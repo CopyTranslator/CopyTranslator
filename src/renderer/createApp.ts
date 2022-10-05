8949971d18ca242ebb991fb64a5f07cbb183ec20
elliott.zheng@foxmail.com <elliottzheng>
2022-10-05 21:04:55 +0800

    fix #448

--- src/renderer/createApp.ts
+++ src/renderer/createApp.ts
@@ -5,6 +5,7 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import { getConfigByKey } from "../store";
 import { ColorMode } from "../common/types";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Vuetify);
 
